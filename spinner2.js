9872a782d79a538c86db68da6f69fd47658667d6
Stanley Lai <stanley.lai@telus.com>
2022-11-22 15:50:00 -0800

    add spinner2

--- spinner2.js
+++ spinner2.js
@@ -1,12 +1,15 @@
 
 
 
-
+let delay = 0;
 let char = ["|", "/", "-", "\\"];
+for (let j = 0; j < 3; j++) {   // 2 rounds of full circle
+  for (let i = 0; i < char.length; i++) {
+    setTimeout(() => {
+      process.stdout.write(`\r${char[i]}   `);
+    }, delay);
+    delay += 200;
+  }
+};
 
-
-
-setTimeout(() => {
-  process.stdout.write('\r|   ');
-}, 100);
 
